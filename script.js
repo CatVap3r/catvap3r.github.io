8eea4b6f40206e06a350b133bf7fd1b4ba33373d
CatVap3r <77308190+CatVap3r@users.noreply.github.com>
2023-04-28 18:08:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ function redirectToWebsiteYT() {
   window.location.href = "https://youtube.com/@catvap3r";
 }
 
+//navbar//
+
 const tButton = document.getElementById('tButton');
 const navbar = document.getElementById('navbar');
 const navButtons = document.querySelectorAll('.navbutton');
+const dcButton = document.getElementById('dcbutton');
+const dcFrame = document.getElementById('dcframe');
+
+//navbar code//
 
 let buttonPressed = false;
 
@@ -12,6 +18,7 @@ tButton.addEventListener('click', () => {
   navbar.classList.toggle('expanded');
   tButton.classList.toggle('expanded');
   navButtons.forEach(button => button.classList.toggle('expanded'));
+  dcFrame.classList.toggle('expanded');
   buttonPressed = !buttonPressed;
-});
 
+});
